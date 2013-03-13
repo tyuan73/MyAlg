1a3dc49e198fb20f4dc169848830465d0611a887
Yuan Tian <tyuan73@hotmail.com>
2013-03-13 13:57:56 -0500

    more leetcode questions solved

---
+++ LeetCode/src/Permutations.java
@@ -0,0 +1,40 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael
+ * Date: 3/12/13
+ * Time: 3:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.*;
+
+public class Permutations {
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        helper(num, 0, ret);
+        return ret;
+    }
+    
+    void helper(int[] num, int index, ArrayList<ArrayList<Integer>> ret) {
+        if(index == num.length) {
+            ArrayList<Integer> list = getList(num);
+            ret.add(list);
+            return;
+        }
+        
+        for(int i = index; i < num.length; i++) {
+            int x = num[i];
+            num[i] = num[index];
+            num[index] = x;
+            helper(num, index+1, ret);
+            num[index] = num[i];
+            num[i] = x;
+        }
+    }
+    
+    ArrayList<Integer> getList(int[] num) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i : num) 
+            list.add(i);
+        return list;
+    }
+}
---
+++ LeetCode/src/PermutationsII.java
@@ -0,0 +1,47 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael
+ * Date: 3/12/13
+ * Time: 3:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.*;
+
+public class PermutationsII {
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        
+        Arrays.sort(num);
+        ret.add(getList(num));
+        
+        int n = num.length;
+        while(true) {
+            int i = n-1;
+            while(i > 0 && num[i-1] >= num[i])
+                i--;
+            if(i == 0)
+                return ret;
+            
+            int j = n-1;
+            while(num[j] <= num[i-1])
+                j--;
+            int x = num[i-1];
+            num[i-1] = num[j];
+            num[j] = x;
+            
+            for(int l = i, r = n-1; l < r; l++, r--) {
+                int y = num[l];
+                num[l] = num[r];
+                num[r] = y;
+            }
+            ret.add(getList(num));
+        }
+    }
+    
+    ArrayList<Integer> getList(int[] num) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i : num)
+            list.add(i);
+        return list;
+    }
+}
