d51ff11f29ad33320b70eee7904dc89a01082392
Michael <tyuan73@gmail.com>
2013-03-15 22:47:29 -0700

    more Leecode problem solved.

---
+++ LeetCode/src/ReverseLinkedListII.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael
+ * Date: 3/15/13
+ * Time: 11:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode p1 = new ListNode(-1);
+        p1.next = head;
+        head = p1;
+        ListNode p2 = p1;
+        for(int i = 0; i < n; i++) {
+            if(i < m-1)
+                p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        for(int i = 0; i < n-m; i++) {
+            ListNode p3 = p1.next;
+            p1.next = p3.next;
+            p3.next = p2.next;
+            p2.next = p3;
+        }
+
+        return head.next;
+    }
+}
---
+++ LeetCode/src/RotateImage.java
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael
+ * Date: 3/16/13
+ * Time: 12:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int l = 0; l < n/2; l++) {
+            int e = n-1-l;
+            int low = l, high = e, left = l, right = e;
+            for(int s1 = l, s2 = l, s3 = e, s4 = e;
+                s1 < e; s1++, s2++, s3--, s4--) {
+                int x = matrix[low][s1];
+                matrix[low][s1] = matrix[s4][left];
+                matrix[s4][left] = matrix[high][s3];
+                matrix[high][s3] = matrix[s2][right];
+                matrix[s2][right] = x;
+            }
+        }
+    }
+}
