7bab929859d1529596fe392c0258b55c37ea3558
Michael <tyuan73@gmail.com>
2013-03-25 16:23:13 -0500

    more leetcode problems solved

--- LeetCode/src/UniquePaths.java
+++ LeetCode/src/UniquePaths.java
@@ -5,6 +5,9 @@
  * Time: 3:44 PM
  * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
  */
+
+import java.util.*;
+
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
@@ -15,5 +18,21 @@ public class UniquePaths {
             }
         }
         return dp[m][n];
+    }
+
+    /**
+     *  Use one dimension array.
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths2(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++) {
+                dp[j] += dp[j-1];
+            }
+        return dp[n-1];
     }
 }
