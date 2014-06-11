cf3f4d4381b2bc6d2452fcec589a6ec30d129ec6
Michael <tyuan73@gmail.com>
2014-06-10 22:02:06 -0500

    commit RoyAndAlphaBetaTree.java

--- HackerRank/src/weekly/week4/day4/RoyAndAlphaBetaTrees.java
+++ HackerRank/src/weekly/week4/day4/RoyAndAlphaBetaTrees.java
@@ -1,167 +1,82 @@
-package weekly.week4.day4;/**
- * Created by yuantian on 6/5/14.
+package weekly.week4.day4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael Tian
+ * Date: 6/5/14
+ * Time: 11:16 PM
+ * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
  */
 
-/*
-
-*/
-
 import java.util.*;
-import java.io.*;
 
 public class RoyAndAlphaBetaTrees {
     public static void main(String[] args) {
-        InputReader in = new InputReader(System.in);
-        OutputWriter out = new OutputWriter(System.out);
+        Scanner in = new Scanner(System.in);
 
+        final long P = 1000000009;
         long[] cat = new long[151];
         cat[0] = 1L;
         for(int i = 1; i < 151; i++) {
             long temp = 0;
             for(int l = 0, r = i-1; r >= 0; l++, r--) {
-                temp += cat[l] * cat[r];
+                temp =  (cat[l] * cat[r] + temp) % P;
             }
             cat[i] = temp;
         }
-        for(int i = 0; i < 151; i++) {
+
+        /*
+        for (int i = 0; i < 151; i++) {
             System.out.println(i + " : " + cat[i]);
         }
-        /*
-        int t = in.readInt();
-        long l = in.readLong();
-        out.printLine(t);
-        out.printLine(l);
-        out.close();
         */
-    }
-}
 
-class InputReader {
-
-    private InputStream stream;
-    private byte[] buf = new byte[1024];
-    private int curChar;
-    private int numChars;
-    private SpaceCharFilter filter;
-
-    public InputReader(InputStream stream) {
-        this.stream = stream;
-    }
-
-    public int read() {
-        if (numChars == -1)
-            throw new InputMismatchException();
-        if (curChar >= numChars) {
-            curChar = 0;
-            try {
-                numChars = stream.read(buf);
-            } catch (IOException e) {
-                throw new InputMismatchException();
+        long[][][] dp = new long[151][151][2];
+        dp[1][1][0] = 1;
+        for(int i = 2; i < 151; i++) {
+            for(int j = 1; j <= i; j++) {
+                int l = j - 1, r = i - j;
+                for(int lIdx = 1; lIdx <= l; lIdx++) {
+                    dp[i][lIdx][0] += (dp[l][lIdx][1] * cat[r]);
+                    dp[i][lIdx][0] %= P;
+                    dp[i][lIdx][1] += (dp[l][lIdx][0] * cat[r]);
+                    dp[i][lIdx][1] %= P;
+                }
+                for(int rIdx = j + 1; rIdx <= i; rIdx++) {
+                    dp[i][rIdx][0] += dp[r][rIdx - j][1] * cat[l];
+                    dp[i][rIdx][0] %= P;
+                    dp[i][rIdx][1] += dp[r][rIdx - j][0] * cat[l];
+                    dp[i][rIdx][1] %= P;
+                }
+                dp[i][j][0] += cat[l]*cat[r];
+                dp[i][j][0] %= P;
             }
-            if (numChars <= 0)
-                return -1;
         }
-        return buf[curChar++];
-    }
 
-    public int peek() {
-        if (numChars == -1)
-            return -1;
-        if (curChar >= numChars) {
-            curChar = 0;
-            try {
-                numChars = stream.read(buf);
-            } catch (IOException e) {
-                return -1;
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            long a = in.nextLong();
+            long b = in.nextLong();
+
+            long[] A = new long[n+1];
+            for(int i = 1; i <= n; i++)
+                A[i] = in.nextLong();
+
+            Arrays.sort(A);
+
+            long res = 0;
+            for(int i = 1; i <= n; i++) {
+                res += (((A[i] * dp[n][i][0]) % P) * a) % P - (((A[i] * dp[n][i][1]) % P) * b) % P;
+                //long temp = dp[n][i][0] * a - dp[n][i][1] * b;
+
+                //res += A[i] * (dp[n][i][0] * a - dp[n][i][1] * b)
+                res %= P;
             }
-            if (numChars <= 0)
-                return -1;
+
+            if (res < 0)
+                res += P;
+            System.out.println(res);
         }
-        return buf[curChar];
-    }
-
-    public int readInt() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        int res = 0;
-        do {
-            if (c < '0' || c > '9')
-                throw new InputMismatchException();
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    public long readLong() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
-        }
-        long res = 0;
-        do {
-            if (c < '0' || c > '9')
-                throw new InputMismatchException();
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
-
-    public boolean isSpaceChar(int c) {
-        if (filter != null)
-            return filter.isSpaceChar(c);
-        return isWhitespace(c);
-    }
-
-    public static boolean isWhitespace(int c) {
-        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-    }
-
-    public boolean isExhausted() {
-        int value;
-        while (isSpaceChar(value = peek()) && value != -1)
-            read();
-        return value == -1;
-    }
-
-    public interface SpaceCharFilter {
-        public boolean isSpaceChar(int ch);
-    }
-}
-
-class OutputWriter {
-    private final PrintWriter writer;
-
-    public OutputWriter(OutputStream outputStream) {
-        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
-    }
-
-    public OutputWriter(Writer writer) {
-        this.writer = new PrintWriter(writer);
-    }
-
-    public void close() {
-        writer.close();
-    }
-
-    public void printLine(long i) {
-        writer.println(i);
-    }
-
-    public void printLine(int i) {
-        writer.println(i);
     }
 }
