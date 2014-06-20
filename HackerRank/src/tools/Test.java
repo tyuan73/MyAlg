3eb567642190392b713be3424c4195494843d004
yuantian <tyuan73@hotmail.com>
2014-06-20 13:58:59 -0500

    update InputReader

--- HackerRank/src/tools/Test.java
+++ HackerRank/src/tools/Test.java
@@ -8,163 +8,172 @@ import java.io.*;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        //Scanner in = new Scanner(new FileInputStream("/home/yuantian/IdeaProjects/MyAlg/HackerRank/src/tools/input.txt"));
-        InputReader in = new InputReader(new FileInputStream("/home/yuantian/IdeaProjects/MyAlg/HackerRank/src/tools/input.txt"));
-        //InputReader in = new InputReader(System.in);
-        //Input input = new Input(new BufferedReader(new FileReader("/home/yuantian/IdeaProjects/MyAlg/HackerRank/src/tools/input.txt")));
+    public static void main(String[] args) throws IOException {
+        String fileName = "/home/yuantian/IdeaProjects/MyAlg/HackerRank/src/tools/input.txt";
 
+        /***************************
+           read with Scanner - slow
+         ****************************/
         long start = System.currentTimeMillis();
+        Scanner inScanner = new Scanner(new FileInputStream(fileName));
+        int n = inScanner.nextInt();
+        while(n-- > 1) {
+            int x = inScanner.nextInt();
+            long y = inScanner.nextLong();
+            String c = inScanner.nextLine();
 
-        /*
-        int n = input.nextInt();
+            //System.out.println("x = " + x + " y = " + y + " c = " + c);
+        }
+        System.out.println("total for Scanner = " + (System.currentTimeMillis() - start));
+
+
+        /***************************
+          input - fast
+        ****************************/
+        start = System.currentTimeMillis();
+        Input input = new Input(new BufferedReader(new FileReader(fileName)));
+        n = input.nextInt();
         while(n-- > 1) {
             int x = input.nextInt();
             long y = input.nextLong();
             String c = input.next();
         }
-        */
-        int n = in.readInt();
-        while(n-- > 1) {
-            int x = in.readInt();
-            long y = in.readLong();
-            String c = in.readString();
-            //System.out.println("x = " + x + " y = " + y + " c = " + c);
-        }
-        /*
-        int n = in.nextInt();
+        System.out.println("total for Input = " + (System.currentTimeMillis() - start));
+
+
+        /***************************
+          input reader - fastest
+         ****************************/
+        start = System.currentTimeMillis();
+        //InputReader in = new InputReader(System.in);
+        InputReader in = new InputReader(new FileInputStream(fileName));
+        n = in.nextInt();
         while(n-- > 1) {
             int x = in.nextInt();
             long y = in.nextLong();
-            String c = in.nextLine();
-
+            String c = in.nextString();
             //System.out.println("x = " + x + " y = " + y + " c = " + c);
         }
-        */
-        System.out.println("total = " + (System.currentTimeMillis() - start));
-    }
-}
-
-class Input {
-    BufferedReader in;
-    StringBuilder sb = new StringBuilder();
-
-    public Input(BufferedReader in) {
-        this.in = in;
+        System.out.println("total for InputReader = " + (System.currentTimeMillis() - start));
     }
 
-    public Input(String s) {
-        this.in = new BufferedReader(new StringReader(s));
-    }
+    static class Input {
+        BufferedReader in;
+        StringBuilder sb = new StringBuilder();
 
-    public String next() throws IOException {
-        sb.setLength(0);
-        while (true) {
-            int c = in.read();
-            if (c == -1) {
-                return null;
+        public Input(BufferedReader in) {
+            this.in = in;
+        }
+
+        public String next() throws IOException {
+            sb.setLength(0);
+            while (true) {
+                int c = in.read();
+                if (c == -1) {
+                    return null;
+                }
+                if (" \n\r\t".indexOf(c) == -1) {
+                    sb.append((char)c);
+                    break;
+                }
             }
-            if (" \n\r\t".indexOf(c) == -1) {
+            while (true) {
+                int c = in.read();
+                if (c == -1 || " \n\r\t".indexOf(c) != -1) {
+                    break;
+                }
                 sb.append((char)c);
-                break;
             }
+            return sb.toString();
         }
-        while (true) {
-            int c = in.read();
-            if (c == -1 || " \n\r\t".indexOf(c) != -1) {
-                break;
-            }
-            sb.append((char)c);
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
         }
-        return sb.toString();
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
     }
 
-    public int nextInt() throws IOException {
-        return Integer.parseInt(next());
-    }
+    static class InputReader {
 
-    public long nextLong() throws IOException {
-        return Long.parseLong(next());
-    }
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
 
-    public double nextDouble() throws IOException {
-        return Double.parseDouble(next());
-    }
-}
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
 
-class InputReader {
-
-    private InputStream stream;
-    private byte[] buf = new byte[1024];
-    private int curChar;
-    private int numChars;
-
-    public InputReader(InputStream stream) {
-        this.stream = stream;
-    }
-
-    public int read() {
-        if (numChars == -1)
-            throw new InputMismatchException();
-        if (curChar >= numChars) {
-            curChar = 0;
-            try {
-                numChars = stream.read(buf);
-            } catch (IOException e) {
+        public int read() {
+            if (numChars == -1)
                 throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
             }
-            if (numChars <= 0)
-                return -1;
+            return buf[curChar++];
         }
-        return buf[curChar++];
-    }
 
-    public int readInt() {
-        return (int) readLong();
-    }
-
-    public long readLong() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
+        public int nextInt() {
+            return (int) nextLong();
         }
-        long res = 0;
-        do {
-            if (c < '0' || c > '9')
-                throw new InputMismatchException();
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
-    }
 
-    public String readString() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        StringBuilder sb = new StringBuilder(1024);
-        do {
-            sb.append((char)c);
-            c = read();
-        } while (!isSpaceChar(c));
-        return sb.toString();
-    }
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
 
-    public static boolean isSpaceChar(int c) {
-        switch (c) {
-            case -1:
-            case ' ':
-            case '\n':
-            case '\r':
-            case '\t':
-                return true;
-            default:
-                return false;
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char)c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 }
