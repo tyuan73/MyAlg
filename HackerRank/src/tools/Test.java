07a406e1da5f8a491954d8058f9fb51334e687ec
yuantian <tyuan73@hotmail.com>
2014-06-20 15:47:47 -0500

    add Input

--- HackerRank/src/tools/Test.java
+++ HackerRank/src/tools/Test.java
@@ -8,7 +8,44 @@ import java.io.*;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        //testReader();
+
+        //testWriter();
+    }
+
+    private static void testWriter() throws IOException {
+        int size = 1000000;
+        long start = System.currentTimeMillis();
+
+        /*
+        for(int i = 0; i < size; i++) {
+            System.out.println(i);
+        }
+        System.out.println("total = " + (System.currentTimeMillis() - start));
+        */
+
+        OutputWriter out = new OutputWriter(System.out);
+        for(int i = 0; i < size; i++) {
+            out.println(i);
+        }
+
+        //out.close();
+        out.println("total = " + (System.currentTimeMillis() - start));
+        out.close();
+
+        /*
+        Output output = new Output(System.out);
+        for(int i = 0; i < size; i++) {
+            output.println(i);
+        }
+        //output.println("total = " + (System.currentTimeMillis() - start));
+        output.flush();
+        System.out.println("total = " + (System.currentTimeMillis() - start));
+        */
+    }
+
+    private static void testReader() throws IOException {
         String fileName = "/home/yuantian/IdeaProjects/MyAlg/HackerRank/src/tools/input.txt";
 
         /***************************
@@ -174,6 +211,78 @@ public class Test {
                 default:
                     return false;
             }
+        }
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void print(long i) {
+            writer.print(i);
+        }
+
+        public void println(long i) {
+            writer.println(i);
+        }
+
+        public void print(int i) {
+            writer.print(i);
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+        public void println() {
+            writer.println();
+        }
+
+        public void print(String s) {
+            writer.print(s);
+        }
+
+        public void println(String s) {
+            writer.println(s);
+        }
+    }
+
+    static class Output {
+        private OutputStream out = null;
+
+        public Output(OutputStream outputStream) {
+            out = outputStream;
+        }
+
+        public void close() throws IOException {
+            out.close();
+        }
+
+        public void flush() throws IOException {
+            out.flush();
+        }
+
+        public void println(long i) throws IOException {
+            out.write((i+"\n").getBytes());
+        }
+
+        public void println(int i) throws IOException {
+            out.write((i+"\n").getBytes());
+        }
+
+        public void println(String s) throws IOException {
+            out.write(s.getBytes());
         }
     }
 }
