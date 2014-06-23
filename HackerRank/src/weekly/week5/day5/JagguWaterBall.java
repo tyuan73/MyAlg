1690a4afa6f327f3d427e7489f8f49d1f3de251d
yuantian <tyuan73@hotmail.com>
2014-06-23 11:49:32 -0500

    week 5 - day5

--- HackerRank/src/weekly/week5/day5/JagguWaterBall.java
+++ HackerRank/src/weekly/week5/day5/JagguWaterBall.java
@@ -5,7 +5,87 @@ package weekly.week5.day5;
  */
 
 /*
+Jaggu is a little kid and he likes playing with water balloons. He took 1 million ( 106 ) empty buckets and he filled the bucket with water balloons under the instruction of his sister Ishika.
+His sister gives him two types of commands:
 
+R pos1 pos2 which implies that jaggu needs to tell her what is the total number of water balloons in the bucket from pos1 to pos2 (both included).
+
+U pos M plus which implies that he has to work like the function
+
+Update(pos,M,plus)
+
+void Update(int pos,int M,int plus)
+{
+    int N=1000000;  //1 million
+    for (int i=1;i<=50;i++)
+    {
+        int back = pos
+        for(int j=1;j<=1000;j++)
+        {
+            add M water ballons at bucket pos
+            int s,in=__builtin_popcount(pos);
+            for(int k=0;;k++)
+            {
+                s=pos+pow(2,k)
+                if( __builtin_popcount(s) <= in )
+                {
+                    in = __builtin_popcount(s)
+                    pos = s;
+                    if(pos>N)       break;
+                    add M water ballons at bucket pos
+                }
+            }
+            pos = pos - N
+        }
+        pos = back+plus;
+        if(pos>N) pos-=N;
+    }
+}
+Jaggu is too lazy to put the water ballons in the bucket. Afraid that he might be caught for not doing what his sister told him to do so, he asks your help to provide correct answers for each of his sister's query. .
+
+Input Format
+
+First line contains Q, number of queries to follow.
+
+Next Q line follows , which can be either an Update Query or Report Query.Each Update Query is followed by atleast 1 report query.
+
+Output Format
+
+For each report query , output the answer in a separate line.
+
+Constraints
+
+1 ≤ Q ≤ 2 * 105
+
+1 ≤ pos1,pos2,pos ≤ 106
+
+pos1 ≤ pos2
+
+1 ≤ M ≤ 10
+
+1 ≤ plus ≤ 999999
+
+Sample Input
+
+2
+U 692778 7 291188
+R 636916 747794
+Sample Output
+
+378
+Explanation
+
+Follow the code above to get the answer.
+
+Note
+
+Input is randomly generated.
+
+__builtin_popcount(x) gives the number of set bits in binary representation of x.
+
+pow(2,k) denotes 2 raised to k , i.e. exponentiation of 2.
+
+Timelimit is 3 times the timelimit mentioned here
 */
 
 import java.util.*;
@@ -17,7 +97,7 @@ public class JagguWaterBall {
     static void go() {
         int q = in.nextInt();
 
-        buckets = new long[1000000];
+        buckets = new long[1000002];
         while (q-- > 0) {
             String command = in.nextString();
             if (command.charAt(0) == 'U') {
@@ -28,10 +108,7 @@ public class JagguWaterBall {
             } else {
                 int p1 = in.nextInt();
                 int p2 = in.nextInt();
-                int total = 0;
-                for (; p1<= p2; p1++)
-                    total += buckets[p1];
-                System.out.println(total);
+                out.println(read(p2) - read(p1-1));
             }
         }
     }
@@ -39,11 +116,13 @@ public class JagguWaterBall {
     static void update(int pos, int m, int plus) {
         int N = 1000000;  //1 million
         for (int i = 1; i <= 50; i++) {
-            System.out.println("i= " + i);
+            //System.out.println("i= " + i);
             int back = pos;
+            //System.out.println("start pos = " + pos);
             for (int j = 1; j <= 1000; j++) {
                 //System.out.println("pos1 = " + pos);
-                buckets[pos] += m;
+                //buckets[pos] += m;
+                add(pos, m);
                 int s, in = getBITs(pos);
                 for (int k = 0; ; k++) {
                     //System.out.println("k = " + k);
@@ -56,15 +135,44 @@ public class JagguWaterBall {
                             break;
                         }
                         //System.out.println("pos2 = " + pos);
-                        buckets[pos] += m;
+                        //buckets[pos] += m;
+                        add(pos, m);
                     }
                 }
+                //System.out.println(" j = " + j + ": " + pos);
                 pos = pos - N;
+                if (pos == 48576 && j < 1000) {
+                    long val = (1000 - j) * m;
+                    add(48576, val);
+                    add(48640, val);
+                    add(49152, val);
+                    add(65536, val);
+                    add(131072, val);
+                    add(262144, val);
+                    add(524288, val);
+                    break;
+                }
             }
             pos = back + plus;
-            System.out.println("pos3 = " + pos);
+            //System.out.println("pos3 = " + pos);
             if (pos > N)
                 pos -= N;
+        }
+    }
+
+    static long read(int idx) {
+        long sum = 0;
+        while(idx > 0) {
+            sum += buckets[idx];
+            idx -= (idx & -idx);
+        }
+        return sum;
+    }
+
+    static void add(int idx, long x) {
+        while(idx <= 1000000) {
+            buckets[idx] += x;
+            idx += (idx & -idx);
         }
     }
 
