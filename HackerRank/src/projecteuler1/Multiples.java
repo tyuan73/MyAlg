0dd3b3c4134e1f57584b8c6cd61b54aa8a8ff0db
yuantian <tyuan73@hotmail.com>
2014-07-07 12:01:36 -0500

    project euler #1

---
+++ HackerRank/src/projecteuler1/Multiples.java
@@ -0,0 +1,138 @@
+package projecteuler1;
+
+/**
+ * Created by yuantian on 7/7/14.
+ */
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below N.
+
+Input Format
+First line contains T, followed by T lines each containing N.
+
+Output Format
+Print the required answer for each test case.
+
+Constraints
+1≤T≤105
+1≤N≤109
+
+Sample Input
+
+2
+10
+100
+Sample Output
+
+23
+2318
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Multiples {
+    static void go() {
+        int t = in.nextInt();
+        while(t-- > 0) {
+            long n = in.nextLong()-1;
+            long x = n/3;
+            long output = (x+1)*x/2 * 3;
+            x = n/5;
+            output += (x+1)*x/2 * 5;
+            x = n/15;
+            output -= (x+1)*x/2 *15;
+            out.println(output);
+        }
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
