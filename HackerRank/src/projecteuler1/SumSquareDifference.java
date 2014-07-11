a646d8cee9dd62cb020182cba0904e9394eb52b1
yuantian <tyuan73@hotmail.com>
2014-07-11 11:26:20 -0500

    Projet Euler - Sum Square Diff

---
+++ HackerRank/src/projecteuler1/SumSquareDifference.java
@@ -0,0 +1,118 @@
+package projecteuler1;
+
+/**
+ * Created by yuantian on 7/11/14.
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class SumSquareDifference {
+    static void go() {
+        long[] d = new long[1001];
+        long c1 = 0, c2 = 0;
+        for(int i = 1; i < 1001; i++) {
+            c2 += c1 * i;
+            c1 += i;
+            d[i] = c2 * 2;
+        }
+
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            out.println(d[n]);
+        }
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
