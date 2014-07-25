2e785500ce0aa4c3970197c407e7c120f30db772
yuantian <tyuan73@hotmail.com>
2014-07-25 09:58:58 -0500

    Projet Euler - #18 maxium path sum 1

---
+++ HackerRank/src/projecteuler1/MaximumPathSum1.java
@@ -0,0 +1,119 @@
+package projecteuler1;
+
+/**
+ * Created by yuantian on 7/25/14.
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class MaximumPathSum1 {
+    static void go() {
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            long[][] table = new long[n+1][n+1];
+            long max = 0;
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= i; j++) {
+                    long x = in.nextLong();
+                    table[i][j] = Math.max(table[i-1][j-1] + x, table[i-1][j] + x);
+                    max = Math.max(max, table[i][j]);
+                }
+            }
+            out.println(max);
+        }
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
