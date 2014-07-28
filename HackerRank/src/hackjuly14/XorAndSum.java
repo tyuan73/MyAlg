d7698cc4cbd0c28471c1a0ebe944fdeb674da510
Michael <tyuan73@gmail.com>
2014-07-27 22:25:09 -0500

    101 hack july 2014 - xorandsum using BigInteger - not working, too slow

---
+++ HackerRank/src/hackjuly14/XorAndSum.java
@@ -0,0 +1,121 @@
+package hackjuly14;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael Tian
+ * Date: 7/26/14
+ * Time: 11:46 PM
+ * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
+ */
+
+/*
+
+*/
+
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+// NOT working
+public class XorAndSum {
+    static void go() {
+        String a = in.nextString();
+        String b = in.nextString();
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger z = BigInteger.ZERO;
+        for(int i = 0; i <= 314159; i++) {
+            z = z.add(x.xor(y.shiftLeft(i)));
+        }
+        int ans = z.mod(new BigInteger("1000000007")).intValue();
+        out.println(ans);
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
