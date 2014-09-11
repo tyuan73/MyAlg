14c72f99d61ade4b50f2860ed499fe970f19def3
yuantian <tyuan73@hotmail.com>
2014-09-11 16:52:30 -0500

    update Palindrome partitioning II

--- LeetCode/src/PalindromePartitioningII.java
+++ LeetCode/src/PalindromePartitioningII.java
@@ -6,6 +6,25 @@
  * To change this template use File | Settings | File Templates.
  */
 public class PalindromePartitioningII {
+    public int minCut(String s) {
+        int n = s.length();
+        // number of cuts for the first k characters
+        int[] dp = new int[n + 1];
+        // init dp so that dp[0] = -1, dp[1] = 0, dp[2] = 1 ....
+        for (int i = 0; i <= n; i++)
+            dp[i] = i - 1;
+        for (int i = 1; i <= n; i++) {
+            // go through all palindromes with odd length that center this char
+            for (int j = 0; i - j >= 0 && i + j < n && s.charAt(i - j) == s.charAt(i + j); j++)
+                dp[i + j + 1] = Math.min(dp[i + j + 1], dp[i - j] + 1);
+            // even length palindrome
+            for (int j = 1; i - j >= 0 && i + j - 1 < n && s.charAt(i - j) == s.charAt(i + j - 1); j++)
+                dp[i + j] = Math.min(dp[i + j], dp[i - j] + 1);
+        }
+        return dp[n];
+    }
+
+    /* this solution didn't pass big test */
     int minPartitionCut(String s) {
         int n = s.length();
         int[] dp = new int[n + 1];
