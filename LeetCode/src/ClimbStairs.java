0ef935db05e425d6441c408b92ca5de3e2c7ba28
yuantian <tyuan73@hotmail.com>
2014-09-15 10:12:03 -0500

    update fibonacci numbers calculation with matrix multiplication

--- LeetCode/src/ClimbStairs.java
+++ LeetCode/src/ClimbStairs.java
@@ -28,4 +28,37 @@ public class ClimbStairs {
 
         return steps[n];
     }
+
+    /**
+     * a solution using matrix multiplication
+     * also in O(log(n))
+     * more generic
+     */
+    public int climbStairs1(int n) {
+        int[][] a = {{0, 1}, {1, 1}};
+        int[][] m = pow(a, n - 1);
+        return m[0][1] + m[1][1];
+    }
+
+    private int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0}, {0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    private int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+            }
+        }
+        return c;
+    }
 }
