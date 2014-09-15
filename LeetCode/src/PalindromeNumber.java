f6cce8b7b5ed31ae1c5230a8ba3ced7c120d8792
yuantian <tyuan73@hotmail.com>
2014-09-15 12:40:18 -0500

    add a new way to check if a number is a palindrome number by reversing only half of it.

--- LeetCode/src/PalindromeNumber.java
+++ LeetCode/src/PalindromeNumber.java
@@ -16,4 +16,34 @@ public class PalindromeNumber {
 
         return reversed == orig;
     }
+
+    /**
+     * another way. It only reverses half of the number and compare them.
+     * note: in leetcode, "0" is palindrome number while "10" is not.
+     */
+    public boolean isPalindrome1(int x) {
+        if (x == 0) return true;
+        // in leetcode, negative numbers and numbers with ending zeros
+        // are not palindrome
+        if (x < 0 || x % 10 == 0)
+            return false;
+
+        // reverse half of the number
+        // the exit condition is y >= x
+        // so that overflow is avoided.
+        int y = 0;
+        while (y < x) {
+            y = y * 10 + (x % 10);
+            if (x == y)  // to check numbers with odd digits
+                return true;
+            x /= 10;
+        }
+        return x == y; // to check numbers with even digits
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumber pn = new PalindromeNumber();
+        System.out.println(pn.isPalindrome(10));
+        System.out.println(pn.isPalindrome1(10));
+    }
 }
