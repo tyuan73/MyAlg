90f9b64405d4d737a133e5b9d3a63ad7ef893eb2
Michael <tyuan73@gmail.com>
2014-09-19 23:35:24 -0500

    CodeSprintIndia 2014

---
+++ HackerRank/src/CodeSprintIndia2014/LargestLexicographicalRotation.java
@@ -0,0 +1,131 @@
+package CodeSprintIndia2014;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael Tian
+ * Date: 9/19/14
+ * Time: 10:38 PM
+ * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class LargestLexicographicalRotation {
+    static void go() {
+        int t = in.nextInt();
+        while(t-- > 0) {
+            String str = in.nextString();
+            int n = str.length();
+            String[] suffix = new String[n];
+            for(int i = 0; i < n; i++) {
+                suffix[i] = str.substring(i)+str.substring(0, i);
+            }
+            Arrays.sort(suffix);
+            out.println(suffix[n-1].substring(0, n));
+        }
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
+
+/*
+input :
+1
+iovnqfnczgnjovvonpdflrxinkderxqwdnmzeeyvmprdmjcz
+
+output:
+ziovnqfnczgnjovvonpdflrxinkderxqwdnmzeeyvmprdmjc
+
+zgnjovvonpdflrxinkderxqwdnmzeeyvmprdmjcziovnqfnc
+* */
---
+++ HackerRank/src/CodeSprintIndia2014/TheRabbit.java
@@ -0,0 +1,122 @@
+package CodeSprintIndia2014;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael Tian
+ * Date: 9/19/14
+ * Time: 11:21 PM
+ * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class TheRabbit {
+    static void go() {
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            long l = 1, r = n;
+            while(l < r) {
+                long mid = (l + r)/2;
+                if ((mid + 3)*mid/2 < n)
+                    l = mid + 1;
+                else
+                    r = mid;
+            }
+            out.println(l);
+        }
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
