a4fb2f742e4d0472b43aa18009e00f7690bd8816
yuantian <tyuan73@hotmail.com>
2014-10-13 16:45:46 -0500

    Tree Pruning

--- HackerRank/src/weekly/week11/day3/TreePruning.java
+++ HackerRank/src/weekly/week11/day3/TreePruning.java
@@ -10,6 +10,37 @@ package weekly.week11.day3;
 
 /*
 
+https://www.hackerrank.com/contests/w11/challenges/tree-pruning
+
+
+Given a tree with N vertices numbered from 1 to N. The vertex 1 is the root of the tree. Each vertex is assigned with an integer weight. A remove operation can remove sub-tree rooted at an arbitrary vertex u. You must use at most K remove operations (possibly zero) so that the total weight of the remaining vertices is largest.
+
+Input Format
+The first line contains two integers N,K.
+The second line contains N integers, the ith integer is the weight of the ith vertex.
+Each of the next N−1 lines contains a pair of integers u and v, which represents an edge from vertex u to vertex v.
+
+Output Format
+Print out a single integer which is the largest total weight of the remaining vertices.
+
+Constraints
+2≤N≤105
+1≤K≤200
+−109≤weight≤109
+Sample Input
+
+5 2
+1 1 -1 -1 -1
+1 2
+2 3
+4 1
+4 5
+Sample Output
+
+2
+Explanation
+We use 2 remove operations: one for the sub-tree rooted at 3 and the other one for the sub-tree rooted at 4. There are only 2 remaining vertices which are 1 and 2. The total weight is 1+1=2.
+
 */
 
 import java.util.*;
@@ -18,33 +49,9 @@ import java.io.*;
 public class TreePruning {
     static List<Integer>[] tree = null;
     static boolean[] visited = null;
-    static int[] parent = null;
     static int[] order = null;
+    static int[] last = null;
     static int index = 0;
-    static int[][] subtree = null;
-    static int[] pri = null;
-
-    static class Ele implements Comparable<Ele> {
-        int index = 0;
-        int pri = 0;
-        long weight = 0;
-
-        public Ele(int i, int pri, long w) {
-            this.index = i;
-            this.pri = pri;
-            this.weight = w;
-
-        }
-
-        public int compareTo(Ele e) {
-            if (this.weight > e.weight)
-                return 1;
-            else if (this.weight < e.weight)
-                return -1;
-            else
-                return e.pri - this.pri;
-        }
-    }
 
     static void go() {
         int n = in.nextInt();
@@ -69,58 +76,40 @@ public class TreePruning {
         visited = new boolean[n];
         order = new int[n];
         index = 0;
-        parent = new int[n];
-        parent[0] = -1;
-        subtree = new int[n][2];
-        pri = new int[n];
+        last = new int[n];
 
         dfs(0);
 
-        PriorityQueue<Ele> pq = new PriorityQueue<Ele>();
-
-        for (int i = n - 1; i > 0; i--) {
-            int v = order[i];
-            w[parent[v]] += w[v];
-            if (w[v] < 0)
-                pq.add(new Ele(v, i, w[v]));
-        }
-        //if (w[0] < 0)
-        //    pq.add(new Ele(0, 0, w[0]));
-
-        while (k > 0 && !pq.isEmpty()) {
-            Ele e = pq.poll();
-            if (e.weight != w[e.index])
-                continue;
-
-            for (int j = subtree[e.index][0]; j <= subtree[e.index][1]; j++) {
-                w[order[j]] = 0;
+        long[] dp = new long[n + 1];
+        for (int i = 1; i < n; i++)
+            dp[i] = dp[i - 1] + w[order[i - 1]];
+        for (int x = 0; x < k; x++) {
+            for (int i = n; i >= 1; i--) {
+                // if node i is not selected, all subtree should be removed,
+                // update the last node to dp[i-1]
+                int lastIndex = i + last[order[i - 1]];
+                dp[lastIndex] = Math.max(dp[lastIndex], dp[i - 1]);
             }
 
-            int idx = e.index;
-            while (parent[idx] != -1) {
-                w[parent[idx]] -= e.weight;
-                if (w[parent[idx]] < 0 && parent[idx] != 0)
-                    pq.add(new Ele(parent[idx], pri[parent[idx]], w[parent[idx]]));
-                idx = parent[idx];
+            for (int i = 1; i <= n; i++) {
+                // if node i is selected, update current value to "dp[i-1] + current weight".
+                dp[i] = Math.max(dp[i], dp[i - 1] + w[order[i - 1]]);
             }
-            k--;
         }
 
-        out.println(Math.max(0, w[0]));
+        out.println(dp[n]);
     }
 
     static void dfs(int v) {
         visited[v] = true;
-        subtree[v][0] = index;
-        pri[v] = index;
+        int start = index;
         order[index++] = v;
         for (int next : tree[v]) {
             if (!visited[next]) {
-                parent[next] = v;
                 dfs(next);
             }
         }
-        subtree[v][1] = index - 1;
+        last[v] = index - start - 1;
     }
 
     static InputReader in;
@@ -211,3 +200,32 @@ public class TreePruning {
         }
     }
 }
+
+
+/*
+input:
+20 5
+773581246 -348306003 -788117784 629111611 -142726426 241605607 418519531 -291199082 -453706450 -850635818 -641575760 453047217 -874946563 -257858612 927125122 860575225 -162713554 61368550 -262466871 361084678
+2 1
+3 2
+4 2
+5 1
+6 3
+7 2
+8 1
+9 6
+10 7
+11 8
+12 1
+13 7
+14 10
+15 3
+16 14
+17 14
+18 15
+19 17
+20 3
+
+output:
+2729019775
+ */
