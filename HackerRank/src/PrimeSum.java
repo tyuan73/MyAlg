0e1ff762a5ef2a6f7154a5ae5307a75019b184f4
yuantian <tyuan73@hotmail.com>
2014-10-13 15:01:23 -0500

    remove global inputreader and printerwriter

--- HackerRank/src/PrimeSum.java
+++ HackerRank/src/PrimeSum.java
@@ -42,12 +42,8 @@ import java.util.*;
 public class PrimeSum {
     static int[] primes = new int[1000009];
     static int total = 0;
-    public static void main(String[] args) {
-        PrimeSum solution = new PrimeSum();
-        solution.go();
-    }
 
-    public void go() {
+    static public void go() {
         InputReader in = new InputReader(System.in);
 
         primes[0]= primes[1]=1;       // we know 0 and 1 are not prime.
@@ -62,10 +58,10 @@ public class PrimeSum {
             }
         }
 
-        int t = in.readInt();
+        int t = in.nextInt();
         while(t-- > 0) {
-            long n = in.readLong();
-            long k = in.readLong();
+            long n = in.nextLong();
+            long k = in.nextLong();
 
             if (n < 2*k) {
                 System.out.println("No");
@@ -112,6 +108,95 @@ public class PrimeSum {
         }
 
         return true;
+    }
+
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
 
