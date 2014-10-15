4705a97c3dc5d42bd0fc2dcdb0ff190cd1d7072d
yuantian <tyuan73@hotmail.com>
2014-10-15 16:35:32 -0500

    maxflow example

---
+++ HackerRank/src/tools/MaxFlow.java
@@ -0,0 +1,13 @@
+package tools;
+
+/**
+ * Created by yuantian on 10/15/14.
+ */
+public class MaxFlow {
+    static public void main(String[] args) {
+        // Maximum Flow:
+        // http://community.topcoder.com/tc?module=Static&d1=tutorials&d2=maxFlow
+
+        // for example of MaxFlow, see weekly.week11.day4.TrainingTheArmy
+    }
+}
