22474f9f54d2909d1bf8c1287ec3e12fe58fac07
yuantian <tyuan73@hotmail.com>
2014-11-07 17:02:03 -0600

    459 D

---
+++ Codeforces/src/problemset459/PashmakAndGraph459E.java
@@ -0,0 +1,158 @@
+package problemset459;
+
+/**
+ * Created by yuantian on 11/7/14.
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class PashmakAndGraph459E {
+    static void go() {
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] g = new int[m][3];
+        for (int i = 0; i < m; i++) {
+            g[i][0] = in.nextInt() - 1;
+            g[i][1] = in.nextInt() - 1;
+            g[i][2] = in.nextInt() - 1;
+        }
+        Arrays.sort(g, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+
+        int[] dp = new int[n];
+        int[] temp = new int[n];
+        for (int i = 0; i < m; /* empty here*/) {
+            int last = i;
+            /* the next 3 for loops are to handle weight with the same value */
+            for (; last < m && g[last][2] == g[i][2]; last++) {
+                temp[g[last][1]] = dp[g[last][1]];
+            }
+
+            for (int j = i; j < last; j++) {
+                temp[g[j][1]] = Math.max(temp[g[j][1]], dp[g[j][0]] + 1);
+            }
+
+            for (int j = i; j < last; j++)
+                dp[g[j][1]] = temp[g[j][1]];
+
+            i = last;
+        }
+
+        int ans = 0;
+        for (int x : dp)
+            ans = Math.max(ans, x);
+        out.println(ans);
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int[] nextIntArray(int len) {
+            int[] ret = new int[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextInt();
+            return ret;
+        }
+
+        public long[] nextLongArray(int len) {
+            long[] ret = new long[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextLong();
+            return ret;
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
