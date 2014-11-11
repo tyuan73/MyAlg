94337f6200570bf31fdcfe771e57bf842fa9557f
Yuan Tian <tyuan73@gmail.com>
2014-11-10 23:16:33 -0600

    478 C

---
+++ Codeforces/src/problemset478/TableDecorations478C.java
@@ -0,0 +1,145 @@
+package problemset478;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yuantian
+ * Date: 11/10/14
+ * Time: 11:03 PM
+ * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class TableDecorations478C {
+    static void go() {
+        /*
+        long total = 0;
+        long[] c = new long[3];
+        for(int i = 0; i < 3; i++) {
+            c[i] = in.nextLong();
+            total += c[i];
+        }
+
+        Arrays.sort(c);
+        long max = total / 3;
+        if (c[0] + c[1] >= max) {
+            out.println(max);
+        } else {
+            out.println(c[0]+c[1]);
+        }
+        */
+
+        // a shorter version
+        long[] color = new long[]{in.nextLong(), in.nextLong(), in.nextLong()};
+        Arrays.sort(color);
+
+        out.println(Math.min((color[0] + color[1] + color[2]) / 3, color[0] + color[1]));
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int[] nextIntArray(int len) {
+            int[] ret = new int[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextInt();
+            return ret;
+        }
+
+        public long[] nextLongArray(int len) {
+            long[] ret = new long[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextLong();
+            return ret;
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
