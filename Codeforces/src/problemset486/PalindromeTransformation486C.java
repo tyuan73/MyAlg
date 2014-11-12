9de1b6df686a875d8926e9778e386cf10413f8ff
yuantian <tyuan73@hotmail.com>
2014-11-11 18:02:41 -0600

    486 C - unfinished

---
+++ Codeforces/src/problemset486/PalindromeTransformation486C.java
@@ -0,0 +1,134 @@
+package problemset486;
+
+/**
+ * Created by yuantian on 11/11/14.
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class PalindromeTransformation486C {
+    static void go() {
+        int n = in.nextInt();
+        int p = in.nextInt();
+        String str = in.nextString();
+        int total = 0;
+        boolean[] v = new boolean[n];
+        for(int i = 0, j = n-1; i < j; i++, j--) {
+            if (str.charAt(i) == str.charAt(j)) {
+                v[i] = true; v[j] = true;
+            } else {
+                total += Math.min((str.charAt(i) + 26 - str.charAt(j)) % 26, (str.charAt(j) + 26 - str.charAt(i)) % 26);
+            }
+        }
+
+
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int[] nextIntArray(int len) {
+            int[] ret = new int[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextInt();
+            return ret;
+        }
+
+        public long[] nextLongArray(int len) {
+            long[] ret = new long[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextLong();
+            return ret;
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
