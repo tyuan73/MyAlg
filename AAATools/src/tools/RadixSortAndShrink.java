c5cf96793d97c09ce192c6420d6b1e1990bf77d4
Yuan Tian <tyuan73@gmail.com>
2014-11-14 00:46:23 -0600

    update Radix Sorting and Shrink

--- AAATools/src/tools/RadixSortAndShrink.java
+++ AAATools/src/tools/RadixSortAndShrink.java
@@ -8,8 +8,22 @@ package tools;/**
 
 import java.util.Scanner;
 
-public class RadixSort {
+public class RadixSortAndShrink {
 
+    public static int[] shrink(int[] a) {
+        int n = a.length;
+        long[] b = new long[n];
+        for (int i = 0; i < n; i++) b[i] = (long) a[i] << 32 | i;
+        b = radixSort(b);
+//		Arrays.sort(b);
+        int[] ret = new int[n];
+        int p = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && (b[i] ^ b[i - 1]) >> 32 != 0) p++;
+            ret[(int) b[i]] = p;
+        }
+        return ret;
+    }
 
     public static long[] radixSort(long[] f) {
         long[] to = new long[f.length];
