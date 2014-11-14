a023a7a93047498fa24920b8e41b8ec43fd99dbe
Yuan Tian <tyuan73@gmail.com>
2014-11-14 01:02:18 -0600

    update shrink

--- Codeforces/src/problemset205/LittleElephantAndCards205D.java
+++ Codeforces/src/problemset205/LittleElephantAndCards205D.java
@@ -49,11 +49,11 @@ public class LittleElephantAndCards205D {
         long[] b = new long[n];
         for (int i = 0; i < n; i++) b[i] = (long) a[i] << 32 | i;
         b = radixSort(b);
-//		Arrays.sort(b);
         int[] ret = new int[n];
         int p = 0;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && (b[i] ^ b[i - 1]) >> 32 != 0) p++;
+        ret[(int) b[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            if (b[i] >> 32 != b[i - 1] >> 32) p++;
             ret[(int) b[i]] = p;
         }
         return ret;
