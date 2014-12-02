fb98e7372f970db5d9c72a0c738832bc6f35a953
yuantian <tyuan73@hotmail.com>
2014-12-02 11:11:18 -0600

    updated 2 solutions

--- LeetCode/src/DecodeWays.java
+++ LeetCode/src/DecodeWays.java
@@ -39,6 +39,32 @@ public class DecodeWays {
     }
 
     /**
+     * DP version - another way
+     */
+    public int numDecodings1(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        int n = s.length();
+        int[] dp = new int[n + 2];
+        dp[n] = 1;
+        int c = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int ch = s.charAt(i) - '0';
+            if (ch == 0 && i > 0 && s.charAt(i - 1) != '1' && s.charAt(i - 1) != '2')
+                return 0;
+            if (ch != 0) {
+                if (ch * 10 + c <= 26) {
+                    dp[i] = dp[i + 1] + dp[i + 2];
+                } else {
+                    dp[i] = dp[i + 1];
+                }
+            }
+            c = ch;
+        }
+        return dp[0];
+    }
+
+    /**
      * Recursive ways. not efficient. Will over time limit if s is considerable long.
      */
     public int numDecodingsRecursive(String s) {
--- LeetCode/src/MinimumDepthOfBinaryTree.java
+++ LeetCode/src/MinimumDepthOfBinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Michael
@@ -6,12 +8,16 @@
  * To change this template use File | Settings | File Templates.
  */
 public class MinimumDepthOfBinaryTree {
+    /**
+     * Solution 1: DFS, recursive
+     */
     public int minDepth(TreeNode root) {
         if (root == null)
             return 0;
 
-        if (root.left == null && root.right == null)
-            return 1;
+        // the next 2 lines are not required.
+        //if (root.left == null && root.right == null)
+        //    return 1;
 
         int left = minDepth(root.left);
         int right = minDepth(root.right);
@@ -19,5 +25,57 @@ public class MinimumDepthOfBinaryTree {
         if (left == 0) return right + 1;
         if (right == 0) return left + 1;
         return Math.min(left, right) + 1;
+    }
+
+    /**
+     * Solution 2: BFS - travel by level, iterative
+     */
+    public int minDepth1(TreeNode root) {
+        if (root == null)
+            return 0;
+        ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+        list.add(root);
+        int depth = 0;
+        while (list.size() > 0) {
+            depth++;
+            ArrayList<TreeNode> next = new ArrayList<TreeNode>();
+            for (TreeNode node : list) {
+                if (node.left == null && node.right == null)
+                    return depth;
+                if (node.left != null)
+                    next.add(node.left);
+                if (node.right != null)
+                    next.add(node.right);
+            }
+            list = next;
+        }
+        return depth;
+    }
+
+    /**
+     * Solution 3: improved DFS, recursive.
+     */
+    int min = 0;
+
+    public int minDepth2(TreeNode root) {
+        if (root == null)
+            return 0;
+        min = Integer.MAX_VALUE;
+        travel(root, 1);
+        return min;
+    }
+
+    private void travel(TreeNode root, int depth) {
+        if (root == null || depth >= min) {
+            return;
+        }
+
+        if (root.left == null && root.right == null) {
+            min = Math.min(min, depth);
+            return;
+        }
+
+        travel(root.left, depth + 1);
+        travel(root.right, depth + 1);
     }
 }
