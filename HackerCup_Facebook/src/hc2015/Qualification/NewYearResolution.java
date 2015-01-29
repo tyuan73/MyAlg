3edf2e444fecec044dab2366ffddd0902cfbdbba
Yuan Tian <tyuan73@gmail.com>
2015-01-28 23:21:36 -0600

    Hackercup 2015 - qualification - q2 - barebone

--- HackerCup_Facebook/src/hc2015/Qualification/NewYearResolution.java
+++ HackerCup_Facebook/src/hc2015/Qualification/NewYearResolution.java
@@ -6,11 +6,56 @@ package hc2015.Qualification;/**
  * Copyright (c) 2013 All Right Reserved, http://github.com/tyuan73
  */
 
+import java.io.*;
 import java.util.*;
 
 public class NewYearResolution {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    FastScanner in;
+    PrintWriter out;
 
+    public void solve() throws IOException {
+
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new File("/Users/yuantian/MyAlg/HackerCup_Facebook/src/hc2015/Qualification/NewYearResolution.in"));
+            out = new PrintWriter(new File("/tmp/test/NewYearResolution.out"));
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] arg) {
+        new CookingTheBooks().run();
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(File file) throws IOException {
+            br = new BufferedReader(new FileReader(file));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "";
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
