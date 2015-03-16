6db68ed8b60c7137deeebadedf909bc1fc54b65a
yuantian <tyuan73@hotmail.com>
2015-03-16 18:00:50 -0500

    problem set 519 A, B, and C

---
+++ Codeforces/src/problemset519/Chess519A.java
@@ -0,0 +1,180 @@
+package problemset519;
+
+/**
+ * Created by yuantian on 3/16/15.
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Chess519A {
+    static void go() {
+        int white = 0, black = 0;
+        for (int i = 0; i < 8; i++) {
+            String str = in.nextString();
+            for (int j = 0; j < 8; j++) {
+                switch (str.charAt(j)) {
+                    case 'Q':
+                        white += 9;
+                        break;
+                    case 'R':
+                        white += 5;
+                        break;
+                    case 'B':
+                        white += 3;
+                        break;
+                    case 'N':
+                        white += 3;
+                        break;
+                    case 'P':
+                        white += 1;
+                        break;
+                    case 'K':
+                        break;
+                    case 'q':
+                        black += 9;
+                        break;
+                    case 'r':
+                        black += 5;
+                        break;
+                    case 'b':
+                        black += 3;
+                        break;
+                    case 'n':
+                        black += 3;
+                        break;
+                    case 'p':
+                        black += 1;
+                        break;
+                    case 'k':
+                        break;
+                }
+            }
+        }
+
+        if (white > black)
+            out.println("White");
+        else if (white < black)
+            out.println("Black");
+        else
+            out.println("Draw");
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int[] nextIntArray(int len) {
+            int[] ret = new int[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextInt();
+            return ret;
+        }
+
+        public long[] nextLongArray(int len) {
+            long[] ret = new long[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextLong();
+            return ret;
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public char[] nextCharArray(int n) {
+            char[] ca = new char[n];
+            for (int i = 0; i < n; i++) {
+                int c = read();
+                while (isSpaceChar(c))
+                    c = read();
+                ca[i] = (char) c;
+            }
+            return ca;
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
