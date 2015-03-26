c21fd4c831fbce97005f11f619f16711f7265525
yuantian <tyuan73@hotmail.com>
2015-03-26 09:32:04 -0500

    update "others"

--- Others/src/Main.java
+++ Others/src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char[] arr = new char[3];
+        for (int i = 0; i < 3; i++)
+            arr[i] = (char) (i + 'a');
+
+        perm(arr, 0);
+    }
+
+    static void perm(char[] arr, int from) {
+        if (from == arr.length) {
+            System.out.println(arr);
+            return;
+        }
+        for (int j = from; j < arr.length; j++) {
+            swap(arr, from, j);
+            perm(arr, from + 1);
+            swap(arr, from, j);
+        }
+    }
+
+    static void swap(char[] arr, int l, int r) {
+        char ch = arr[l];
+        arr[l] = arr[r];
+        arr[r] = ch;
     }
 }
---
+++ Others/src/TestInteger.java
@@ -0,0 +1,18 @@
+/**
+ * Created by yuantian on 1/26/15.
+ */
+public class TestInteger {
+
+    public static void main(String[] args) {
+        Integer a = new Integer(1);
+        Integer b = new Integer(1);
+        int c = 1;
+
+        if (a == b) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
+    }
+}
