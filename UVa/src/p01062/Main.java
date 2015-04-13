f25f58f33b2898bea85b05f17a2024d1399d970d
yuantian <tyuan73@hotmail.com>
2015-04-13 14:21:32 -0500

    UVa - 732 (solved) & 1062

--- UVa/src/p01062/Main.java
+++ UVa/src/p01062/Main.java
@@ -1,7 +1,7 @@
-package p00732;
+package p01062;
 
 /**
- * Created by yuantian on 4/10/15.
+ * Created by yuantian on 4/13/15.
  */
 
 /*
@@ -12,70 +12,30 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static String l1 = null, l2 = null;
-    static char[] output = null;
-    static char[] stack = null; // stack to hold chars
-    static int top = 0;         // the top of the stack
-
     static void go() {
-        while (true) {
-            try {
-                l1 = in.nextString();
-                l2 = in.nextString();
-            } catch (Exception e) {
-                break;
-            }
-            if (l1.length() != l2.length()) {
-                out.println("[\n]");
-                continue;
-            }
-            out.println('[');
-
-            output = new char[2 * l1.length()];
-            stack = new char[l1.length() + 1];
-            top = 1;
-            check(-1, 0, 0);
-
-            out.println(']');
-        }
-
-    }
-
-    static void check(int i1, int i2, int idx) {
-        if (i2 >= l2.length()) {
-            printLine();
-            return;
-        }
-
-        char ch = l2.charAt(i2);
-        for (int i = l1.length() - 1; i > i1; i--) {
-            if (l1.charAt(i) == ch) {
-                int k = idx;
-                int pre = top;
-                for (int j = i1 + 1; j < i; j++) {
-                    stack[top++] = l1.charAt(j);
-                    output[k++] = 'i';
+        String line = null;
+        int cs = 1;
+        while (!(line = in.nextString()).equals("end")) {
+            boolean[] stack = new boolean[26];
+            for (int i = 0; i < line.length(); i++) {
+                int ch = line.charAt(i) - 'A';
+                for (int j = ch; j < 26; j++) {
+                    if (stack[j]) {
+                        stack[j] = false;
+                        break;
+                    }
                 }
-                output[k++] = 'i';
-                output[k++] = 'o';
-                check(i, i2 + 1, k);
-                top = pre;
+                stack[ch] = true;
             }
-        }
-        if (stack[top - 1] == ch) {
-            char c = stack[--top];
-            output[idx++] = 'o';
-            check(i1, i2 + 1, idx);
-            stack[top++] = c;
-        }
-    }
 
-    static void printLine() {
-        StringBuilder sb = new StringBuilder();
-        for (char x : output) {
-            sb.append(' ').append(x);
+            int ans = 0;
+            for (boolean s : stack)
+                if (s) ans++;
+
+            out.println("Case " + cs + ": " + ans);
+            cs++;
         }
-        out.println(sb.substring(1));
+
     }
 
     static InputReader in;
