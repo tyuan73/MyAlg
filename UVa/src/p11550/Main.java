a2d67316a4996e7446a7ec3c1db7b19473803b86
yuantian <tyuan73@hotmail.com>
2015-04-20 10:15:59 -0500

    UVa - 11550 AC

---
+++ UVa/src/p11550/Main.java
@@ -0,0 +1,175 @@
+package p11550;
+
+/**
+ * Created by yuantian on 4/20/15.
+ */
+
+/*
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static void go() {
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int[] count = new int[m];
+            int[] pattern = new int[m];
+            int[] map = new int[1024];   // more than enough
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (1 == in.nextInt()) {
+                        count[j]++;
+                        pattern[j] |= 1 << i;
+                    }
+                }
+            }
+            boolean yes = true;
+            // check if each edge has 2 vertices
+            for (int x : count) {
+                if (x != 2) {
+                    yes = false;
+                    break;
+                }
+            }
+            // check if there are more that 1 edges between 2 vertices
+            if (yes) {
+                for (int x : pattern) {
+                    if (++map[x] > 1) {
+                        yes = false;
+                        break;
+                    }
+                }
+            }
+            out.println(yes ? "Yes" : "No");
+        }
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int[] nextIntArray(int len) {
+            int[] ret = new int[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextInt();
+            return ret;
+        }
+
+        public long[] nextLongArray(int len) {
+            long[] ret = new long[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextLong();
+            return ret;
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextLine() {
+            StringBuilder sb = new StringBuilder(1024);
+            int c = read();
+            while (!(c == '\n' || c == '\r')) {
+                sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public char[] nextCharArray(int n) {
+            char[] ca = new char[n];
+            for (int i = 0; i < n; i++) {
+                int c = read();
+                while (isSpaceChar(c))
+                    c = read();
+                ca[i] = (char) c;
+            }
+            return ca;
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
