ec63061a514798f21ded875326d1776e43737356
yuantian <tyuan73@hotmail.com>
2016-07-28 14:40:06 -0500

    leetcode - OJ188 - Best time to by stock

--- LeetCode/src/OJ188BestTimeToBuyStockIV.java
+++ LeetCode/src/OJ188BestTimeToBuyStockIV.java
@@ -1,8 +1,14 @@
 /**
  * Created by yuantian on 2/11/16.
  */
+
+import java.util.*;
+
 public class OJ188BestTimeToBuyStockIV {
-    public int maxProfit(int k, int[] prices) {
+    /*
+    DP version. Runtime O(kn)
+     */
+    public int maxProfit1(int k, int[] prices) {
         int len = prices.length;
 
         // this is very important! Otherwise, you will get TLE
@@ -28,5 +34,39 @@ public class OJ188BestTimeToBuyStockIV {
         for (int x : dp)
             max = Math.max(max, x);
         return max;
+    }
+
+    public int maxProfit(int k, int[] prices) {
+        List<Integer> profit = new ArrayList<>();
+
+        int curMax = 0, cur = 0;
+        for (int i = 1; i < prices.length; i++) {
+            cur += prices[i] - prices[i - 1];
+            curMax = Math.max(curMax, cur);
+            if (cur <= 0) {
+                if (curMax > 0) profit.add(curMax);
+                curMax = 0;
+                cur = 0;
+            }
+        }
+        if (cur > 0)
+            profit.add(curMax);
+
+        System.out.println("profit size = " + profit.size());
+        Collections.sort(profit, Collections.reverseOrder());
+        k = Math.min(k, profit.size());
+        int ans = 0;
+        for (int i = 0; i < k; i++) {
+            ans += profit.get(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int k = 2;
+        int[] prices = {0,0,0,0,0,0,0,0};
+        //int[] prices = {1, 2, 3, 4, 5, 0, 7, 8, 9};
+        OJ188BestTimeToBuyStockIV test = new OJ188BestTimeToBuyStockIV();
+        System.out.println(test.maxProfit(k, prices));
     }
 }
