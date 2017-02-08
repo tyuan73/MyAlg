5fd7b29250adb62d0002f95874d10ab02be5d5d4
yuantian <tyuan73@hotmail.com>
2017-02-08 14:02:25 -0600

    leetcode - 179 - java 8

--- LeetCode/src/LargestNumber179.java
+++ LeetCode/src/LargestNumber179.java
@@ -26,4 +26,13 @@ public class LargestNumber179 {
         if (ans.charAt(0) == '0') return "0";
         return ans;
     }
+
+    /**
+     * Java 8
+     */
+    public String largestNumber1(int[] num) {
+        String[] array = Arrays.stream(num).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(array, (String s1, String s2) -> (s2 + s1).compareTo(s1 + s2));
+        return Arrays.stream(array).reduce((x, y) -> x.equals("0") ? y : x + y).get();
+    }
 }
