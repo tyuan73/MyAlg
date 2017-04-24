ff336103a0a0afe7f0cc6470c604837a650b9ff3
tyuan73 <tyuan73@hotmail.com>
2017-04-24 13:35:26 -0500

    leetcode - 564 palindrome numbers - add comments

--- LeetCode/src/FindtheClosestPalindrome564.java
+++ LeetCode/src/FindtheClosestPalindrome564.java
@@ -3,12 +3,18 @@
  */
 public class FindtheClosestPalindrome564 {
     public String nearestPalindromic(String n) {
+        // the length of the left (head) part, and the length of the right (tail) part
         int left = (n.length() + 1) / 2, right = n.length() - left;
-        long head = Long.parseLong(n.substring(0, left));
-        long origin = Long.parseLong(n);
+        long head = Long.parseLong(n.substring(0, left)); // head in number
+        long origin = Long.parseLong(n); // original number
         long diff = Long.MAX_VALUE, num = 0;
+        /**
+         * in order to get closest palindrome, we just need to check palindrome
+         * numbers that can be constructed from: (left - 1), (left + 0) and
+         * (left + 1).
+         */
         for (int i = -1; i <= 1; i++) {
-            long ret = getPalindrom(head + i, right);
+            long ret = getPalindrome(head + i, right);
             if (ret != origin && Math.abs(ret - origin) < diff) {
                 diff = Math.abs(ret - origin);
                 num = ret;
@@ -17,13 +23,29 @@ public class FindtheClosestPalindrome564 {
         return Long.toString(num);
     }
 
-    private long getPalindrom(long head, int rightLen) {
+    /**
+     * Construct palindrome from head. Don't need to check if the length of the original
+     * number is odd or not. The right part must have the same length as the right part of the
+     * original number.
+     */
+    private long getPalindrome(long head, int rightLen) {
         String str = Long.toString(head);
         StringBuilder sb = new StringBuilder(str);
+        /**
+         * Corner cases to consider:
+         * 1. numbers like "10" has a left of "1" which will be "0" when checking (head - 1);
+         * 2. numbers like "1000" has a left of "10" which will be "9" when checking (head - 1),
+         *    this is the case that left has a shorter length than right;
+         *
+         * By checking (head == 0) || rightLen > str.length(), we kill them all.
+         */
         if (head == 0l || rightLen > str.length()) {
             for (int i = 0; i < rightLen; i++)
                 sb.append('9');
         } else {
+            /**
+             * Construct the palindrome numbers from head. This is pretty straight forward.
+             */
             for (int i = rightLen - 1; i >= 0; i--)
                 sb.append(str.charAt(i));
         }
