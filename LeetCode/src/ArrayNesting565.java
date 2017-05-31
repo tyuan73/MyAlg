f42e8e1667a1cb321ba6d2329656eeb6cf4cc15f
tyuan73 <tyuan73@hotmail.com>
2017-05-31 08:44:04 -0500

    Leetcode - 565 - disjoint set - a minor improvement

--- LeetCode/src/ArrayNesting565.java
+++ LeetCode/src/ArrayNesting565.java
@@ -18,7 +18,7 @@ public class ArrayNesting565 {
         Arrays.fill(ds, -1);
         int min = -1;
         for (int i = 0; i < n; i++) {
-            int a = find(ds, i), b = find(ds, nums[i]);
+            int a = find(ds, i), b = find(ds, nums[i]); // "a = find(ds, i)" can be removed if we redesign how ds gets updated. See below.
             if (a != b) {
                 ds[a] += ds[b];
                 ds[b] = a;
@@ -30,5 +30,24 @@ public class ArrayNesting565 {
 
     private int find(int[] ds, int i) {
         return ds[i] < 0 ? i : (ds[i] = find(ds, ds[i]));
+    }
+
+    /**
+     * The same idea. Improved a little bit by removing "a = find(ds, i)" from above solution.
+     */
+    public int arrayNesting1(int[] nums) {
+        int n = nums.length;
+        int[] ds = new int[n];
+        Arrays.fill(ds, -1);
+        int min = -1;
+        for (int i = 0; i < n; i++) {
+            int a = find(ds, nums[i]);
+            if (a != i) {
+                ds[a] += ds[i];
+                ds[i] = a;
+                min = Math.min(min, ds[a]);
+            }
+        }
+        return -min;
     }
 }
