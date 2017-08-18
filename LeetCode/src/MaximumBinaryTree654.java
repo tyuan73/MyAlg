06bfbb2eeb5b4a003a2c6f356d96f701a2444510
tyuan73 <tyuan73@hotmail.com>
2017-08-18 10:43:45 -0500

    Leetcode - 654 - add O(n) solution

--- LeetCode/src/MaximumBinaryTree654.java
+++ LeetCode/src/MaximumBinaryTree654.java
@@ -1,3 +1,6 @@
+
+import java.util.*;
+
 public class MaximumBinaryTree654 {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         TreeNode root = null;
@@ -15,5 +18,23 @@ public class MaximumBinaryTree654 {
         }
         node.right = insert(node.right, x);
         return node;
+    }
+
+    /**
+     * Use stack to keep the right most path from root to the right end.
+     */
+    public TreeNode constructMaximumBinaryTre1e(int[] nums) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode fakeRoot = new TreeNode(Integer.MAX_VALUE);
+        stack.push(fakeRoot);
+        for (int x : nums) {
+            while (x > stack.peek().val)
+                stack.pop();
+            TreeNode node = new TreeNode(x);
+            node.left = stack.peek().right;
+            stack.peek().right = node;
+            stack.push(node);
+        }
+        return fakeRoot.right;
     }
 }
