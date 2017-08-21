7ccaa1ece487c8a1a67c0c21f60482a883b20b34
tyuan73 <tyuan73@hotmail.com>
2017-08-21 10:58:50 -0500

    Leetcode - 663 - HashSet + DFS - reformat

--- LeetCode/src/EqualTreePartition663.java
+++ LeetCode/src/EqualTreePartition663.java
@@ -3,11 +3,12 @@ import java.util.*;
 
 public class EqualTreePartition663 {
     Set<Integer> set = null;
+
     public boolean checkEqualTree(TreeNode root) {
         set = new HashSet<>();
         int sum = sum(root.left) + sum(root.right) + root.val;
         if ((sum & 1) == 1) return false;
-        return set.contains(sum/2);
+        return set.contains(sum / 2);
     }
 
     private int sum(TreeNode node) {
