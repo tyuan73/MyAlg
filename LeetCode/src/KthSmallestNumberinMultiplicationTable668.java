c68adf7fa6399e8db10902e277da5235225df066
tyuan73 <tyuan73@hotmail.com>
2017-09-25 09:00:54 -0500

    Leetcode - 668 - O(n*lg(n)) solution

--- LeetCode/src/KthSmallestNumberinMultiplicationTable668.java
+++ LeetCode/src/KthSmallestNumberinMultiplicationTable668.java
@@ -8,7 +8,6 @@ import java.io.*;
 public class KthSmallestNumberinMultiplicationTable668 {
     /**
      * First try. LTE
-     * @return
      */
     public int findKthNumber(int m, int n, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> ((a[0] + 1) * (a[1] + 1) - (b[0] + 1) * (b[1] + 1)));
@@ -20,6 +19,29 @@ public class KthSmallestNumberinMultiplicationTable668 {
             if (num[0] < m - 1) pq.add(new int[]{num[0] + 1, num[1]});
         }
         return (num[0] + 1) * (num[1] + 1);
+    }
+
+    /**
+     * O(n * lg(n))
+     */
+    public int findKthNumber1(int m, int n, int k) {
+        int l = 1, h = m * n;
+        while (l < h) {
+            int mid = (h + l) / 2;
+            if (enough(mid, m, n, k))
+                h = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    private boolean enough(int x, int m, int n, int k) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(n, x / i);
+        }
+        return count >= k;
     }
 
     /**
