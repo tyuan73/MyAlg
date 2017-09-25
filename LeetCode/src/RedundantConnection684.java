2462259d802352a9c9aa245e71cfb73a2b5783ac
Yuan <tyuan73@gmail.com>
2017-09-24 21:34:29 -0500

    Leetcode - 684 - minor update

--- LeetCode/src/RedundantConnection684.java
+++ LeetCode/src/RedundantConnection684.java
@@ -8,10 +8,8 @@ import java.io.*;
 public class RedundantConnection684 {
     public int[] findRedundantConnection(int[][] edges) {
         int[] ds = new int[2001];
-        Arrays.fill(ds, -1);
         for (int[] e : edges) {
-            int p1 = find(ds, e[0]);
-            int p2 = find(ds, e[1]);
+            int p1 = find(ds, e[0]), p2 = find(ds, e[1]);
             if (p1 == p2) return e;
             ds[p1] = p2;
         }
@@ -19,6 +17,6 @@ public class RedundantConnection684 {
     }
 
     private int find(int[] ds, int idx) {
-        return ds[idx] == -1 ? idx : (ds[idx] = find(ds, ds[idx]));
+        return ds[idx] == 0 ? idx : (ds[idx] = find(ds, ds[idx]));
     }
 }
