b5ee01778bd46c5233e12a1b7a4ddc1792a90200
Yuan <tyuan73@gmail.com>
2017-10-01 00:13:12 -0500

    Leetcode - 688 - improved with 2D array

--- LeetCode/src/KnightProbabilityinChessboard688.java
+++ LeetCode/src/KnightProbabilityinChessboard688.java
@@ -8,6 +8,9 @@ import java.io.*;
 public class KnightProbabilityinChessboard688 {
     static final int[][] steps = {{2, 1}, {-2, 1}, {2, -1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
 
+    /**
+     * One Dimension array.
+     */
     public double knightProbability(int N, int K, int r, int c) {
         double ans = 0.0;
         double[] count = new double[N * N];
@@ -33,4 +36,29 @@ public class KnightProbabilityinChessboard688 {
         return ans;
     }
 
+    /**
+     * 2D array solution. much easier
+     */
+    public double knightProbability2D(int N, int K, int r, int c) {
+        double ans = 0.0;
+        double[][] count = new double[N][N];
+        count[r][c] = 1;
+        while (K-- > 0) {
+            double[][] next = new double[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    for (int[] s : steps) {
+                        int x = i + s[0], y = j + s[1];
+                        if (x >= 0 && x < N && y >= 0 && y < N)
+                            next[x][y] += count[i][j] / 8;
+                    }
+                }
+            }
+            count = next;
+        }
+        for (double[] d : count)
+            for (double d1 : d)
+                ans += d1;
+        return ans;
+    }
 }
