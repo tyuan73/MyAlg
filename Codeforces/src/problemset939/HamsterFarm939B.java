2c1427704d39ed414360041fa14386df976301b5
tyuan73 <tyuan73@hotmail.com>
2018-02-28 12:44:43 -0600

    Codeforces - 939 A, B, C, D, E

---
+++ Codeforces/src/problemset939/HamsterFarm939B.java
@@ -0,0 +1,158 @@
+package problemset939;
+
+
+/*
+
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class HamsterFarm939B {
+    static void go() {
+        long n = in.nextLong();
+        int k = in.nextInt();
+        long[] a = in.nextLongArray(k);
+        long min = n;
+        int idx = 0;
+        for (int i = 0; i < k; i++) {
+            long x = n % a[i];
+            if (x < min) {
+                min = x;
+                idx = i;
+            }
+        }
+        out.println((idx + 1) + " " + (n / a[idx]));
+    }
+
+    static InputReader in;
+    static PrintWriter out;
+
+    public static void main(String[] args) {
+        in = new InputReader(System.in);
+        out = new PrintWriter(System.out);
+
+        go();
+
+        out.close();
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int[] nextIntArray(int len) {
+            int[] ret = new int[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextInt();
+            return ret;
+        }
+
+        public long[] nextLongArray(int len) {
+            long[] ret = new long[len];
+            for (int i = 0; i < len; i++)
+                ret[i] = nextLong();
+            return ret;
+        }
+
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public long nextLong() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String nextLine() {
+            StringBuilder sb = new StringBuilder(1024);
+            int c = read();
+            while (!(c == '\n' || c == '\r' || c == -1)) {
+                sb.append((char) c);
+                c = read();
+            }
+            return sb.toString();
+        }
+
+        public char nextChar() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            return (char) c;
+        }
+
+        public String nextString() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder sb = new StringBuilder(1024);
+            do {
+                sb.append((char) c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return sb.toString();
+        }
+
+        public char[] nextCharArray(int n) {
+            char[] ca = new char[n];
+            for (int i = 0; i < n; i++) {
+                int c = read();
+                while (isSpaceChar(c))
+                    c = read();
+                ca[i] = (char) c;
+            }
+            return ca;
+        }
+
+        public static boolean isSpaceChar(int c) {
+            switch (c) {
+                case -1:
+                case ' ':
+                case '\n':
+                case '\r':
+                case '\t':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
